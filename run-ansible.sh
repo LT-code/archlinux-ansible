7f1151d276884c56e862346310c267649fcdde99
Thomas Lopez <thomas.lopez@kbrwadventure.com>
2024-09-25 15:29:17 +0200

    Refactored archlinux ansible setup script

---
+++ run-ansible.sh
@@ -0,0 +1,4 @@
+cd ansible
+#git clone https://github.com/kewlfft/ansible-aur.git ~/.ansible/plugins/modules/aur
+ansible-galaxy collection install kewlfft.aur
+ansible-playbook -u root -e host=$(cat /etc/hostname) --ask-vault-pass -i hosts local.yml
